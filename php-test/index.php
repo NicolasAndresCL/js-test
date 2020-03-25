caeeaca43e82b74a9f892239aefb9123a47e76e3
Giovanny Cano <giovannycanoleal@gmail.com>
2020-03-25 19:38:08 -0300

    example GET-POST with PHP

---
+++ php-test/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="mostrar.php" method="post">
+
+    <label for="">name</label>
+    <input name="name" type="text" /><br/>
+    <label for="">email</label>
+    <input name="email" type="email" /><br/>
+    <label for="">message</label>
+    <textarea name="message" ></textarea>
+    <input type="submit" value="SEND" class="submit" />
+            
+</form>
+
+
+</body>
+</html>
---
+++ php-test/mostrar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <label for="name">Nombre: <?php echo $_POST['name'] ?></label><br>
+    <label for="email">Email: <?php echo $_POST['email'] ?></label><br>
+    <label for="message">Mensaje: <?php echo $_POST['message'] ?></label><br>
+    <a href="http://localhost:8080/js-test/php-test/">Volver</a>
+    <?php
+        // $_GET['']
+        // print_r($_GET);
+    ?>
+
+
+
+</body>
+</html>
