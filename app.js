c63a17b50cbf6c2946aa3291fd04d0eed32aa0ff
Nicolás Cano <nicolas_8228@hotmail.com>
2020-03-25 18:25:31 -0400

    Mi primer Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+function imprimir( persona ){
+
+	 console.log(persona.nombre + " " + persona.apellido);
+}
+
+
+var obj = {
+    nombre: "Nicolás",
+    apellido: "Cano"
+
+
+}
+
+imprimir(obj);     
+
+
+
+
+
+
